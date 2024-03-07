b8693fc2c56ce5b53b9a772dbf3da0b59e474a74
Pranshu Verma <pranshuverma1601@gmail.com>
2024-03-07 15:54:50 +0530

    bug fixed

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,6 +1,9 @@
 import { Outlet, useNavigate } from "react-router-dom";
 const Auth = () => {
   const navigate = useNavigate();
+  if(localStorage.getItem("token")) {
+    navigate("/");
+  }
   return (
     <>
       <div className="w-full h-screen flex items-center justify-center">
